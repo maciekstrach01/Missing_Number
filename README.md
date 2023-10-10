e03f0033563406f7b3445f384d01d3a8de55f3f6
Maciej Strach <maciej.strach01@gmail.com>
2023-10-10 19:44:57 +0200

    Add

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# Missing number 
+
+This code aims to find the missing number in two arrays of integers.
+
+## 🛠 Skills
+C#
+
+
+## Screenshots
+
+![missing number](https://github.com/maciekstrach01/Library_Management/assets/146733279/bf5d2531-c62e-46f9-8dc1-b198a7ea63fd)
+
+
+
+
+
+
+
+
+
+
+
+## Running Tests
+
+To run the tests, you must have Visual Studio or another preferred IDE for C# installed. Configure the IDE for C#, translate the code into machine language i.e. compile it, and then run the program.
+
+```
+
---
+++ main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        int[] tab1 = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        int[] tab2 = {10, 11, 12, 13, 14, 15, 16, 17, 18, 20};
+        int missingNumber = 0;
+        
+        for(int i = 0; i < tab1.Length; i++) {
+            missingNumber ^= tab1[i];
+        }
+        for(int i = 0; i < tab2.Length; i++) {
+            missingNumber ^= tab2[i];
+        }
+
+            Console.WriteLine("The contents of table tab1:");
+        foreach (var item in tab1)
+        {
+            Console.Write(item + " ");
+        }
+
+        Console.WriteLine("\nThe contents of table tab2:");
+        foreach (var item in tab2)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine("\nThe missing number is: " + missingNumber);
+    }
+}
+
+
+
